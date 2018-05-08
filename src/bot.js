e0363fa7aa234f9b7123900b55829b8cb727224d
JcluoIvan <jcluo.ivan@gmail.com>
2018-05-08 16:58:13 +0800

    update

--- src/bot.js
+++ src/bot.js
@@ -6,22 +6,24 @@ const token = '444322366:AAEMUKhyjaZjD8uNCe_hag2h79HSPe6CPO0';
 const execSync = require('child_process').execSync;
 
 const bot = new TelegramBot(token, {polling: true});
-let sidAutoincrement = 0;
+// let sidAutoincrement = 0;
 
 let app = {
     users: [],
     connects: [],
     offlines: [],
 
-    serviceConnect (io, name) {
+    serviceConnect (io, {name, cmd, cwd, pid}) {
         let address = io.remoteAddress;
         let connectAt = moment();
-
+        // let sid = ++ sidAutoincrement;
         app.connects.unshift({
-            sid: ++ sidAutoincrement,
+            // sid,
             io,
             address,
-            restartCommand: {cmd: null, cwd: null},
+            cmd,
+            cwd,
+            pid,
             name,
             connectAt,
         });
@@ -30,7 +32,7 @@ let app = {
         io.on('error', err => app.serviceDisconnect(io, err));
 
         /* 若是監視狀態時, 傳送訊息 */
-        let message = `[ ${name} ] connect at ${connectAt.format('HH:mm:ss')}`;
+        let message = `[${pid}] ${name} connect at ${connectAt.format('HH:mm:ss')}`;
         this.users.filter(u => u.watch).forEach(user => {
             bot.sendMessage(user.id, message);
         });
@@ -40,23 +42,38 @@ let app = {
     serviceDisconnect (io, error = null) {
 
         let connect = this.connects.find(o => o.io === io);
-        let {sid, restartCommand, name, connectAt} = connect;
+        let {cmd, cwd, name, connectAt} = connect;
 
         let disconnectAt = moment();
 
         /* 移除連線 */
         this.connects = this.connects.filter(c => c !== connect);
 
-        this.offlines.unshift({
-            sid,
-            restartCommand,
-            name,
+        let off = this.offlines.find(o => o.name === name);
+
+        if (! off) {
+            off = {
+                cmd,
+                cwd,
+                name,
+                pid: 0,
+                connectAt: 0,
+                disconnectAt: 0,
+                records: [],
+            };
+            this.offlines.push(off);
+        }
+
+        off.connectAt = connectAt;
+        off.disconnectAt = disconnectAt;
+        off.records.push({
+            error,
             connectAt,
             disconnectAt,
-            error
         });
 
-        let message = `[ ${name} ] disconnect at ${disconnectAt.format('HH:mm:ss')}`;
+
+        let message = `${name} are disconnect at ${disconnectAt.format('HH:mm:ss')}`;
         this.users.filter(u => u.watch).forEach(user => {
             bot.sendMessage(user.id, message);
         });
@@ -101,27 +118,18 @@ const server = new net.createServer(client => {
     /* 斷線後不嘗試重新連線 */
     io.reconnecting = false;
 
-    /* 限制必需在 1000 內設定名稱, 不然就切斷 */
+    /* 限制必需在 1秒 內設定名稱, 不然就切斷 */
     let timer = setTimeout(() => {
         io.emit('error', 'no set name');
         io.disconnect();
+        timer = null;
     }, 1000);
 
-    /* 設定名稱 */
-    io.on('name', value => {
-        if (! timer) {
-            return;
-        }
-        app.serviceConnect(io, value);
+    /* 程序註冊 */
+    io.on('register', data => {
+        app.serviceConnect(io, data);
         clearTimeout(timer);
         timer = null;
-    });
-
-    io.on('restart.command', ({cmd, cwd}) => {
-        let conn = app.connects.find(o => o.io === io);
-        if (conn) {
-            conn.restartCommand = {cmd, cwd};
-        }
     });
 
 });
@@ -131,16 +139,16 @@ server.listen(8800, () => {
     console.log('server start');
 });
 
-bot.onText(/\/get (.+)/, (msg, match) => {
+bot.onText(/\/(\w+)$/, (msg, match) => {
     let id = msg.chat.id;
 
     switch (match[1]) {
 
         /* 列出所有連線服務 */
-        case 'list': {
-            let message = ['程序 ID. [程序名稱] at 連線建立時間'];
+        case 'online': {
+            let message = ['[PID] 程序名稱 at 連線建立時間'];
             app.connects.forEach(o => {
-                message.push(`[ ${o.name} ] at ${o.connectAt.format('MM-DD HH:mm:ss')}`);
+                message.push(`[${o.pid}] ${o.name} at ${o.connectAt.format('MM-DD HH:mm:ss')}`);
             });
             if (message.length > 1) {
                 message = message.join('\n');
@@ -153,82 +161,118 @@ bot.onText(/\/get (.+)/, (msg, match) => {
         /* 列出所有離線的服務 */
         case 'offline': {
 
-            let message = ['SID. [程序名稱] at 離線建立時間(MM-DD HH:mm:ss)'];
+            let message = ['程序名稱 at 離線建立時間(MM-DD HH:mm:ss)'];
             app.offlines.forEach(o => {
-                message.push(`${o.sid}. [${o.name}] at ${o.disconnectAt.format('MM-DD HH:mm:ss')}`);
+                message.push(`${o.name} at ${o.disconnectAt.format('MM-DD HH:mm:ss')}`);
             });
             if (message.length > 1) {
                 message = message.join('\n');
             } else {
-                message = '目前無程序在線';
+                message = '目前無程序離線';
             }
             bot.sendMessage(id, message);
         } break;
+
+        /* 啟用監視狀態 */
+        case 'watch':
+            app.userWatch(id);
+            bot.sendMessage(id, 'watch on');
+            break;
+
+        /* 關閉監視狀態 */
+        case 'unwatch':
+            app.userUnWatch(id);
+            bot.sendMessage(id, 'watch off');
+            break;
+
+        case 'help':
+
+            bot.sendMessage(id, ([
+                '/online  > 取得所有連線中的服務',
+                '/offline  >  取得離線的服務',
+                '/restart {name}  >  重啟離線的服務',
+                '/delete {name} > 停止服務',
+                '/watch  >  開始關注提醒',
+                '/unwatch  >  停止關注',
+                '/help  >  列出所有功能'
+            ]).join('\n'));
+
+            break;
+
         default:
             console.info(`unknown command > ${match[1]}`);
             break;
     }
 });
 
-bot.onText(/\/watch/, msg => {
-    let id = msg.chat.id;
-
-    /* 啟用監視狀態 */
-    app.userWatch(id);
-    bot.sendMessage(id, 'watch on');
-});
-
-bot.onText(/\/unwatch/, msg => {
-    let id = msg.chat.id;
-    /* 關閉監視狀態 */
-    app.userUnWatch(id);
-    bot.sendMessage(id, 'watch off');
-});
-
-bot.onText(/\/restart (.+)/, (msg, match) => {
+bot.onText(/\/(re|restart) (.+)/, (msg, match) => {
 
     let id = msg.chat.id;
-    let sid = Number(match[0]) || 0;
+    let name = match[2] || null;
 
-    let item = app.offlines.find(o => o.sid === sid);
+    if (! name) {
+        bot.sendMessage(id, `請輸入程序名稱`);
+    }
+
+    let item = app.connects.find(o => o.name === name) || app.offlines.find(o => o.name === name);
     if (! item) {
-        bot.sendMessage(id, `服務名稱 [${name}] 不存在, 請檢查名稱是否有誤`);
+        bot.sendMessage(id, `服務 ${name} 不存在, 請檢查名稱是否有誤`);
         return;
     }
 
-    let {cwd, cmd} = item.restartCommand;
-    if (! cmd) {
+    if (! item.cmd) {
         bot.sendMessage(id, `服務未設定重啟命令, 無法重啟`);
         return;
     }
 
-    let repeat = app.connects.find(o => (o.name === item.name) || (o.restartCommand.cmd === cmd));
-    if (repeat) {
-        bot.sendMessage(id, `重啟項目與目前線上服務 [${repeat.name}] 的名稱 or 重啟指令重複, 無法執行指令`);
+    if (item.pid) {
+        try {
+            execSync(`kill ${item.pid}`);
+        } catch (err) {
+            bot.sendMessage(id, `服務重啟失敗 (無法停止程序 pid ${item.pid}) > ${err.toString()}`);
+        }
+    }
+
+    // let repeat = app.connects.find(o => (o.name === item.name) || (o.cmd === cmd));
+    // if (repeat) {
+    //     console.info(repeat.pid);
+    //     bot.sendMessage(id, `重啟項目與目前線上服務 [${repeat.name}] 的名稱 or 重啟指令重複, 無法執行指令`);
+    //     return;
+    // }
+
+    /* 建立 log 時間名稱 */
+    const timeTag = moment().format('YYMMDD-HHss');
+    try {
+        const outName = `${item.name}.${timeTag}.out`;
+        /* 重新啟動服務 */
+        let result = execSync(`nohup ${item.cmd}&>${outName}&`, {cwd: item.cwd});
+        bot.sendMessage(id, `指令 [ ${item.cwd}/${item.cmd} | out=${item.cwd}/${outName} ] 執行成功 > ${result.toString()}`);
+    } catch (err) {
+        bot.sendMessage(id, `服務重啟失敗 (啟動執行執行失敗)> ${err.toString()}`);
+    }
+});
+
+bot.onText(/\/(del|delete) (.+)/, (msg, match) => {
+
+    let id = msg.chat.id;
+    let name = match[2] || null;
+
+    if (! name) {
+        bot.sendMessage(id, `請輸入程序名稱`);
+    }
+
+    let item = app.connects.find(o => o.name === name) || app.offlines.find(o => o.name === name);
+    if (! item) {
+        bot.sendMessage(id, `服務 ${name} 不存在, 請檢查名稱是否有誤`);
         return;
     }
 
-    /* 重新啟動服務 */
-    try {
-        let result = execSync(`nohup ${cmd} &`, {cwd});
-        bot.sendMessage(id, `指令執行成功 > ${result}`);
-    } catch (err) {
-        bot.sendMessage(id, `服務重啟失敗 > ${err.toString()}`);
+    if (item.pid) {
+        try {
+            execSync(`kill ${item.pid}`);
+        } catch (err) {
+            bot.sendMessage(id, `服務停止失敗 (無法停止程序 pid ${item.pid}) > ${err.toString()}`);
+        }
     }
-});
-
-bot.onText(/\/help/, msg => {
-    let id = msg.chat.id;
-    let message = ([
-        '/get list  > 取得所有連線中的服務',
-        '/get offline  >  取得離線的服務',
-        '/restart {sid}  >  重啟離線的服務',
-        '/watch  >  開始關注提醒',
-        '/unwatch  >  停止關注',
-        '/help  >  列出所有功能'
-    ]);
-    bot.sendMessage(id, message.join('\n'));
 
 });
-
-
